dce8914c26b51cfa5b95a8173616fd07e18ef1ae
ViniciusGCaetano <viny6888@gmail.com>
2022-12-27 11:56:12 -0300

    adding minimal code to trace error on binance api

--- streamlit_homebroker/app/api_connections/get_order_data.py
+++ streamlit_homebroker/app/api_connections/get_order_data.py
@@ -1,8 +1,11 @@
 import requests
 import json
+import streamlit as st
 
 def get_orders(pair, depth):
     response = requests.get(f'https://api.binance.com/api/v3/depth?symbol={pair}&limit={depth}')
     byte_json = response.content
     orders_json = json.loads(byte_json)
     return orders_json
+
+st.write(get_orders('BTCBUSD', 5))
