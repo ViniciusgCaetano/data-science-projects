c207433bbfc47b0e02e05aa88af0c88f4b0612e2
ViniciusGCaetano <viny6888@gmail.com>
2022-12-28 03:26:17 -0300

    Testing again

--- streamlit_homebroker/app/api_connections/get_order_data.py
+++ streamlit_homebroker/app/api_connections/get_order_data.py
@@ -3,7 +3,7 @@ import json
 import streamlit as st
 
 def get_orders(pair, depth):
-    response = requests.get(f'https://api.binance.com/api/v3/depth?symbol={pair}&limit={depth}')
+    response = requests.get(f'https://data.binance.com/api/v3/depth?symbol={pair}&limit={depth}')
     byte_json = response.content
     orders_json = json.loads(byte_json)
     return orders_json
